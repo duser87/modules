f5d7dce55cc991f7a10f83394d4f8f36888b6d25
Sh.Sergei <duser87@yandex.ru>
2024-12-24 23:01:49 +0300

    homework05.2

---
+++ homework05.2/src/main/java/ru/innopolis/Main.java
@@ -0,0 +1,81 @@
+package ru.innopolis;
+
+
+import ru.innopolis.entity.WorkFiles;
+
+import java.util.*;
+
+public class Main {
+
+    public static void main(String[] args) {
+
+        String[] reader = WorkFiles.readFileStream("homework05.2/output.txt");
+
+        List<Integer> intList  = new ArrayList<>();
+
+        List<Integer> uniqueIntList  = new ArrayList<>();
+
+        Integer[] intArr = new Integer[reader.length - 1];
+
+        int count = reader.length - 1;
+
+        Integer intBuff;
+
+        for (String s: reader){
+            intList.add(Integer.parseInt(s));
+        }
+
+        WorkFiles.writeFileStream("homework05.2/input.txt", String.valueOf(intList));
+
+        System.out.println(intList);
+
+        Collections.sort(intList);
+
+        WorkFiles.writeFileStream("homework05.2/input.txt", String.valueOf(intList));
+
+        System.out.println(intList);
+
+        intList.sort(Comparator.reverseOrder());
+
+        WorkFiles.writeFileStream("homework05.2/input.txt", String.valueOf(intList));
+
+        System.out.println(intList);
+
+        for(Integer i: intList){
+
+            if(Collections.frequency(intList, i) > 1){
+
+                uniqueIntList.add(i);
+
+            }
+
+        }
+
+        System.out.println(uniqueIntList);
+
+        WorkFiles.writeFileStream("homework05.2/input.txt", String.valueOf(uniqueIntList));
+
+
+        Map<String, String> map = new HashMap<>();
+
+        map.put("Str", "String");
+
+        map.put("int", "int");
+
+        if(map.containsKey("Str")){
+
+            String newStr = (String) map.get("Str") + ", string2";
+
+            map.put("Str", newStr);
+
+        }
+
+        System.out.println(map.keySet());
+
+        map.remove("int");
+
+        System.out.println(map.toString());
+
+
+    }
+}
---
+++ homework05.2/src/main/java/ru/innopolis/entity/WorkFiles.java
@@ -0,0 +1,48 @@
+package ru.innopolis.entity;
+
+import java.io.*;
+import java.nio.charset.StandardCharsets;
+
+public class WorkFiles{
+
+    static public String[] readFileStream(String path){
+
+        String str;
+        String[]  arrStr;
+
+        try (BufferedReader bufferedReader = new BufferedReader(new FileReader(path))){
+
+            str = bufferedReader.readLine();
+
+            arrStr = str.split(", ");
+
+        }
+        catch (IOException e){
+
+            throw new RuntimeException(e);
+
+        }
+
+        return arrStr;
+
+    }
+
+    static public void writeFileStream(String pathFile, String str){
+
+        try (FileWriter writer =  new FileWriter(pathFile, true)){
+
+            writer.write(str);
+            writer.append('\n');
+
+            writer.flush();
+
+        }
+        catch (IOException e){
+
+            throw new RuntimeException(e);
+
+        }
+
+    }
+
+}
