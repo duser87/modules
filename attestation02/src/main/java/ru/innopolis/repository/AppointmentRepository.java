13547743d62b4ab31cb3b62069bc31008a79bc17
Sh.Sergei <duser87@yandex.ru>
2025-01-07 23:51:33 +0300

    attestation02 v 2.0

--- attestation02/src/main/java/ru/innopolis/repository/AppointmentRepository.java
+++ attestation02/src/main/java/ru/innopolis/repository/AppointmentRepository.java
@@ -3,18 +3,19 @@ package ru.innopolis.repository;
 import ru.innopolis.model.Appointment;
 
 import java.util.List;
+import java.util.Optional;
 
 public interface AppointmentRepository {
 
-    public String create();
+    public Boolean create(Long id, String fio_d, String fio_p, String t, String desc);
 
-    public Appointment findById(Long id);
+    public Optional<Appointment> findById(Long id);
 
     public List<Appointment> findAll();
 
     public String update(Long id, String fio_d, String fio_p, String time, String desc);
 
-    public String deleteById(Long id);
+    public void deleteById(Long id);
 
     public String deleteAll();
 
--- attestation02/src/main/java/ru/innopolis/repository/implementation/AppointmentRepoImpl.java
+++ attestation02/src/main/java/ru/innopolis/repository/implementation/AppointmentRepoImpl.java
@@ -1,34 +1,103 @@
 package ru.innopolis.repository.implementation;
 
+import org.springframework.jdbc.core.JdbcTemplate;
+import org.springframework.jdbc.core.RowMapper;
+import org.springframework.jdbc.core.simple.JdbcClient;
+import ru.innopolis.App;
+import ru.innopolis.JDBCTemplateDriver;
 import ru.innopolis.model.Appointment;
 import ru.innopolis.repository.AppointmentRepository;
 
 import java.util.List;
+import java.util.Optional;
 
 public class AppointmentRepoImpl implements AppointmentRepository {
+
+    JdbcTemplate template = JDBCTemplateDriver.jdbcTemplate();
+    JdbcClient templateClient = JdbcClient.create(JDBCTemplateDriver.jdbcTemplate());
+
+    private static final String CREATE = "INSERT INTO appointments (id, id_d, id_p, time, description) VALUES (?, (SELECT id FROM doctors WHERE fio_d = ?), (SELECT id FROM patients WHERE fio = ?), ?, ?)";
+
+    private static final String FINDBYID = "SELECT * FROM appointments WHERE id = ?";
+
+    private static final String FINDALL = "SELECT * FROM appointments";
+
+    private static final String UPDATE = "UPDATE appointments SET id_d = ? , id_p = ? , time = ? , description = ? WHERE id = ?";
+
+    private static final String DELETEBYID = "DELETE FROM appointments WHERE id = ?";
+
+    private static final String DELETEALL = "DELETE FROM appointments";
+
+    private static final String COUNTBYID = "SELECT COUNT(*) FROM appointments WHERE id_d = ?";
+
     @Override
-    public String create() {
-        return "";
+    public Boolean create(Long id, String fio_d, String fio_p, String t, String desc) {
+
+        Optional<Appointment> appointment = templateClient.sql(CREATE)
+                .param(id)
+                .param(fio_d)
+                .param(fio_p)
+                .param(t)
+                .param(desc)
+                .query(Appointment.class)
+                .optional();
+
+        return appointment.isPresent();
     }
 
     @Override
-    public Appointment findById(Long id) {
-        return null;
+    public Optional<Appointment> findById(Long id) {
+
+        return Optional.of(templateClient.sql(FINDBYID)
+                .param(id)
+                .query(Appointment.class)
+                .optional().orElseThrow());
+
     }
 
     @Override
     public List<Appointment> findAll() {
-        return List.of();
+
+        return template.query(FINDALL, appointmentRowMapper);
+
     }
 
     @Override
     public String update(Long id, String fio_d, String fio_p, String time, String desc) {
-        return "";
+
+        Long rowCount = 0L;
+
+        rowCount = template.queryForObject("SELECT COUNT(*) FROM appointments WHERE id = ?", Long.class, id);
+
+        Optional.ofNullable(rowCount).ifPresentOrElse(
+
+                (X) -> {
+
+                    template.update(UPDATE, fio_d, fio_p, time, desc, id);
+
+                },
+
+                () -> {
+
+                    template.update(CREATE, id, fio_d, fio_p, time, desc);
+
+                }
+
+        );
+
+        return rowCount > 0 ? "Обновлена запись с ID = " + id.toString() : "Добавлена запись с ID = " + id.toString();
     }
 
     @Override
-    public String deleteById(Long id) {
-        return "";
+    public void deleteById(Long id) {
+
+        Optional<Appointment> x = templateClient.sql(DELETEBYID)
+                .param(id)
+                .query(Appointment.class)
+                .optional();
+
+        x.orElseThrow();
+
     }
 
     @Override
@@ -40,4 +109,17 @@ public class AppointmentRepoImpl implements AppointmentRepository {
     public Long countById(Long id) {
         return 0L;
     }
+
+    private static final RowMapper<Appointment> appointmentRowMapper = (row, rowNumber) -> {
+
+        Long id = row.getLong("id");
+        Long id_d = row.getLong("id_d");
+        Long id_p = row.getLong("id_p");
+        String time = row.getString("time");
+        String desc = row.getString("description");
+
+        return new Appointment(id, id_d, id_p, time, desc);
+
+    };
+
 }
