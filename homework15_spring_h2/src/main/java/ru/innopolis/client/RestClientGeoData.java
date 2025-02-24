e0fffb8450ca91490b4591c91794152c66d7679d
Sh.Sergei <duser87@yandex.ru>
2025-02-24 21:07:28 +0300

    Homeworks/homework15_spring_h2 v.1.7

--- homework15_spring_h2/src/main/java/ru/innopolis/client/RestClientGeoData.java
+++ homework15_spring_h2/src/main/java/ru/innopolis/client/RestClientGeoData.java
@@ -1,20 +1,27 @@
 package ru.innopolis.client;
 
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.core.ParameterizedTypeReference;
 import org.springframework.http.HttpStatus;
 import org.springframework.stereotype.Component;
 import org.springframework.web.client.RestClient;
 import ru.innopolis.dto.EarthquakeDataRemoteHostResponse;
+import ru.innopolis.entity.EarthquakeEntity;
 import ru.innopolis.repository.EarthquakeRepository;
+import ru.innopolis.service.EarthquakeService;
 
 import java.nio.file.NoSuchFileException;
+import java.time.Instant;
+import java.time.LocalDateTime;
+import java.time.ZoneId;
 import java.util.*;
 
+@Slf4j
 @Component
 public class RestClientGeoData implements CommandLineRunner {
 
-    private EarthquakeRepository repository;
+    private final EarthquakeRepository repository;
 
     public RestClientGeoData(EarthquakeRepository repo){
         repository = repo;
@@ -23,15 +30,22 @@ public class RestClientGeoData implements CommandLineRunner {
     @Override
     public void run(String... args) throws Exception {
         var res = getDataGeoJson();
-        System.out.println(res);
-
+        log.info(res.toString());
+        List<EarthquakeEntity> list = res.getFeatures().stream().map( x -> EarthquakeEntity.builder()
+                .title(x.getProperties().getTitle())
+                .mag(x.getProperties().getMag())
+                .place(x.getProperties().getPlace())
+                .time(LocalDateTime.ofInstant(Instant.ofEpochMilli(x.getProperties().getTime()), ZoneId.systemDefault()))
+                .build()).toList();
+        log.info(list.toString());
+        repository.saveAll(list);
     }
 
 
 
-    private List<EarthquakeDataRemoteHostResponse> getDataGeoJson(){
+    private EarthquakeDataRemoteHostResponse getDataGeoJson(){
 
-        ParameterizedTypeReference<List<EarthquakeDataRemoteHostResponse>> typeRef = new ParameterizedTypeReference<>(){};
+        ParameterizedTypeReference<EarthquakeDataRemoteHostResponse> typeRef = new ParameterizedTypeReference<>(){};
         var result = RestClient.builder().build();
         var x =  result
                 .get()
@@ -46,7 +60,6 @@ public class RestClientGeoData implements CommandLineRunner {
             }
             return null;
         });
-        System.out.println(x.get(0).getFeatures().get(0).getProperties());
         return  x;
     }
 
