050a59023ecc88092415a91cc2b9e6cc8d26f5e1
Sh.Sergei <duser87@yandex.ru>
2025-03-27 21:00:14 +0300

    Homeworks/homework23_spring_security_auth_from_db_v_1.1

--- homework23_spring_auth_from_db/src/main/java/ru/innopolis/config/SecurityConfig.java
+++ homework23_spring_auth_from_db/src/main/java/ru/innopolis/config/SecurityConfig.java
@@ -6,14 +6,23 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.security.core.userdetails.User;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.provisioning.JdbcUserDetailsManager;
+import org.springframework.security.provisioning.UserDetailsManager;
 import org.springframework.security.web.SecurityFilterChain;
+
+import javax.sql.DataSource;
+import java.util.List;
 
 @Configuration
 @RequiredArgsConstructor
 @EnableWebSecurity
 public class SecurityConfig {
+
+    private final DataSource dataSource;
 
     @Bean
     PasswordEncoder passwordEncoder(){
@@ -34,5 +43,12 @@ public class SecurityConfig {
                 .formLogin(Customizer.withDefaults())
                 .build();
     }
+
+//    @Bean
+//    public UserDetailsManager user(PasswordEncoder encoder){
+//        var manager = new JdbcUserDetailsManager(dataSource);
+//        manager.createUser(new User("userT", encoder.encode("123456"), List.of(new SimpleGrantedAuthority("USER")) ));
+//        return manager;
+//    }
 
 }
