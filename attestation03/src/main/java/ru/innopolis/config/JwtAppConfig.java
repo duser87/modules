b229e3d5376bb46295a921d812766df7e4472cfe
Sh.Sergei <duser87@yandex.ru>
2025-04-07 22:21:41 +0300

    Homeworks/homework23_spring_security_roles_v_1.5

--- attestation03/src/main/java/ru/innopolis/config/JwtAppConfig.java
+++ attestation03/src/main/java/ru/innopolis/config/JwtAppConfig.java
@@ -2,12 +2,19 @@ package ru.innopolis.config;
 
 import lombok.RequiredArgsConstructor;
 import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.authentication.AuthenticationProvider;
+import org.springframework.security.authentication.dao.DaoAuthenticationProvider;
+import org.springframework.security.config.annotation.authentication.configuration.AuthenticationConfiguration;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Component;
 import ru.innopolis.repository.JpaUserRepository;
 
-@Component
+@Configuration
 @RequiredArgsConstructor
 public class JwtAppConfig {
 
@@ -16,5 +23,23 @@ public class JwtAppConfig {
     @Bean
     public UserDetailsService userDetailsService(){
         return username -> jpaUserRepository.findByEmail(username).orElseThrow(() -> new UsernameNotFoundException("--->>> Такого пользователя нет!"));
+    }
+
+    @Bean
+    public AuthenticationProvider authenticationProvider(){
+        DaoAuthenticationProvider authProvider = new DaoAuthenticationProvider();
+        authProvider.setUserDetailsService(userDetailsService());
+        authProvider.setPasswordEncoder(passwordEncoder());
+        return authProvider;
+    }
+
+    @Bean
+    public AuthenticationManager authenticationManager(AuthenticationConfiguration authenticationConfiguration) throws Exception{
+        return authenticationConfiguration.getAuthenticationManager();
+    }
+
+    @Bean
+    public PasswordEncoder passwordEncoder(){
+        return new BCryptPasswordEncoder();
     }
 }
--- attestation03/src/main/java/ru/innopolis/config/SecurityConfig.java
+++ attestation03/src/main/java/ru/innopolis/config/SecurityConfig.java
@@ -1,37 +1,67 @@
 package ru.innopolis.config;
 
 import lombok.RequiredArgsConstructor;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.authentication.AuthenticationProvider;
+import org.springframework.security.config.annotation.method.configuration.EnableMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
 import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
+import org.springframework.security.web.authentication.logout.LogoutHandler;
+import ru.innopolis.entity.Role;
+
+import static org.springframework.http.HttpMethod.*;
+import static org.springframework.security.config.http.SessionCreationPolicy.STATELESS;
 
 @Configuration
 @RequiredArgsConstructor
 @EnableWebSecurity
+@EnableMethodSecurity
 public class SecurityConfig {
 
+    private static final String[] LIST_URL = {"/api/v1/auth/**",
+            "/patient",
+            "/patient/**",
+            "/doctor",
+            "/doctor/**",
+            "/appointment",
+            "/appointment/**"};
+
     private final JwtAuthenticationFilter jwtAuthFilter;
+    private final AuthenticationProvider authenticationProvider;
+    private final LogoutHandler logoutHandler;
 
     @Bean
     public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception{
         http
-                .csrf()
-                .disable()
-                .authorizeHttpRequests()
-                .requestMatchers("")
-                .permitAll()
-                .anyRequest()
-                .authenticated()
-                .and()
-                .sessionManagement()
-                .sessionCreationPolicy(SessionCreationPolicy.STATELESS)
-                .and()
+                .csrf(AbstractHttpConfigurer::disable)
+                .authorizeHttpRequests(req ->
+                        req.requestMatchers(LIST_URL)
+                                .permitAll()
+                                .requestMatchers("/api/v1/appointment/**").hasAnyAuthority()
+                                .requestMatchers(GET, "/api/v1/appointment/**").hasAnyAuthority()
+                                .requestMatchers(POST, "/api/v1/appointment/**").hasAnyAuthority()
+                                .requestMatchers(PUT, "/api/v1/appointment/**").hasAnyAuthority()
+                                .requestMatchers(DELETE, "/api/v1/appointment/**").hasAnyAuthority()
+                                .anyRequest()
+                                .authenticated()
+                )
+                .sessionManagement(session -> session.sessionCreationPolicy(STATELESS))
                 .authenticationProvider(authenticationProvider)
                 .addFilterBefore(jwtAuthFilter, UsernamePasswordAuthenticationFilter.class)
+                .logout(logout ->
+                        logout.logoutUrl("/api/v1/auth/logout")
+                                .addLogoutHandler(logoutHandler)
+                                .logoutSuccessHandler((request, response, authentication) -> SecurityContextHolder.clearContext())
+                )
+        ;
+        return http.build();
 
     }
 }
