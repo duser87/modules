a478787602b4db2a88697492f7c202b128a57da5
Sh.Sergei <duser87@yandex.ru>
2025-06-12 23:19:40 +0300

    Homeworks/attestation05_v.1.3

--- attestation05/src/main/java/ru/innopolis/repositories/JpaAppointmentRepository.java
+++ attestation05/src/main/java/ru/innopolis/repositories/JpaAppointmentRepository.java
@@ -6,9 +6,11 @@ import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 import ru.innopolis.models.Entity.AppointmentEntity;
 
+import java.util.List;
+
 @Repository
 public interface JpaAppointmentRepository extends JpaRepository<AppointmentEntity, Long> {
 
-    @Query(value = "SELECT a FROM AppointmentEntity a WHERE a.idEmpl=:idEmpl AND a.idPat=:idPat")
-    AppointmentEntity findByIdEmplAndIdPat(@Param("idEmpl") Long idEmpl, @Param("idPat") Long idPat);
+    @Query(value = "SELECT a FROM AppointmentEntity a WHERE a.idEmpl=:idEmpl AND a.idPat=:idPat AND a.time=:time")
+    public AppointmentEntity findByIdEmplAndIdPat(@Param("idEmpl") Long idEmpl, @Param("idPat") Long idPat, @Param("time") String time);
 }
--- attestation05/src/main/java/ru/innopolis/services/AppointmentService.java
+++ attestation05/src/main/java/ru/innopolis/services/AppointmentService.java
@@ -3,49 +3,12 @@ package ru.innopolis.services;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Service;
-import ru.innopolis.models.DTO.AppointmentDTO;
-import ru.innopolis.models.Entity.AppointmentEntity;
-import ru.innopolis.repositories.JpaAppointmentRepository;
-import ru.innopolis.repositories.JpaEmployeeRepository;
-import ru.innopolis.repositories.JpaPatientRepository;
+
 
 @Slf4j
 @Service
 @RequiredArgsConstructor
 public class AppointmentService {
-
-    private final JpaAppointmentRepository jpaAppointmentRepository;
-    private final JpaEmployeeRepository jpaEmployeeRepository;
-    private final JpaPatientRepository jpaPatientRepository;
-
-    public String create(AppointmentDTO aDTO){
-//        log.info(aDTO.toString());
-//        var resAppointment = jpaAppointmentRepository.findByIdEmplAndIdPat(aDTO.getIdEmpl(), aDTO.getIdPat(), aDTO.getTime());
-//        log.info(resAppointment.toString());
-//        if (resAppointment != null){
-//
-//        }
-//        jpaAppointmentRepository.save(ae);
-//        return "Вы записаны к врачу " +  " на " + ae.getTime();
-    }
-
-    public AppointmentEntity findById(Long id){
-        return jpaAppointmentRepository.findById(id).orElseThrow();
-    }
-
-    public String delete(Long id){
-        jpaAppointmentRepository.deleteById(id);
-        return "Ваша запись к врачу " + " удалена!";
-    }
-
-    public String update(AppointmentEntity ae){
-        var res = jpaAppointmentRepository.findById(ae.getId()).orElseThrow();
-        res.setDescription(ae.getDescription());
-        res.setIdEmpl(ae.getIdEmpl());
-        res.setIdPac(ae.getIdPac());
-        jpaAppointmentRepository.save(res);
-        return "Запись с ID-" + ae.getId() + " изменена!";
-    }
 
 
 }
